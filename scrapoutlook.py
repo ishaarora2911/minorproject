cf458b8f6aab158e631961d1a978819817b11925
isha <ishaarora2911@gmail.com>
2023-03-12 15:31:14 +0530

    fetching internal url's and data

--- scrapoutlook.py
+++ scrapoutlook.py
@@ -10,10 +10,30 @@ soup = BeautifulSoup(response.text, "html.parser")
 # print(soup.title)
 li=[]
 ni=[]
-for link in soup.find_all('a',
-                          attrs={'href': re.compile("^https://skilloutlook.com/phd-research/")}):
-    li.append(link.get('href'))
+for i in soup.find_all('div',id='entryContainer'):
+    for link in i.find_all('a',
+                          attrs={'href': re.compile("^https://skilloutlook.com/")}):
+        li.append(link.get('href'))
 print(li)
+for j in range(0,1):
+    i=li[j]
+    print("link=",i)
+    url2=i
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.49"}
+    response2 = requests.get(url2, headers=headers)
+    soups = BeautifulSoup(response2.text, "html.parser")
+    for j in soups.find_all('div',id='entryContainer'):
+        # for data in j.find_all('div',id='entryContainer'):
+        for j2 in j.find_all('p'):
+        #     pattern = re.compile("https://",j2)
+        #     if pattern:
+        #         pass
+        #     else:
+                print("1:",j2.text)
+
+    #print(response2.text)
+
 #     print(link)
 #     display the actual urls
 #     print(link.get('href'))
